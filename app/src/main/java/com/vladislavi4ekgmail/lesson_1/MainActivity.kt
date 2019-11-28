ac3367bccb2c8fdbd46a20b0a35a3fce906346bf
vladislavi4ek <rusguba@mail.ru>
2019-11-28 03:08:53 +0300

    Lesson_1 new project

---
+++ app/src/main/java/com/vladislavi4ekgmail/lesson_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vladislavi4ekgmail.lesson_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
